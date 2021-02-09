9853e1937e84285d03399879deb2c46d688ede48
Justin Appelgren <justinappelgren@gmail.com>
2021-02-09 14:03:51 -0600

    Form stores all values in state except ingredients

--- src/components/NewRecipeForm/NewRecipeForm.js
+++ src/components/NewRecipeForm/NewRecipeForm.js
@@ -5,8 +5,12 @@ export default function NewRecipeForm({ closeModal }) {
     const [newRecipe, setNewRecipe] = useState(newRecipeDefaultState)
 
     const handleChange = (event) => {
-        setNewRecipe({ ...newRecipe, [event.target.name]: event.target.value })
-        console.log(newRecipe)
+        if (event.target.name === 'servings') {
+            setNewRecipe({ ...newRecipe, [event.target.name]: Number(event.target.value) })
+        } else {
+            setNewRecipe({ ...newRecipe, [event.target.name]: event.target.value })
+        }
+
     }
 
     const handleSubmit = (event) => {
@@ -23,12 +27,12 @@ export default function NewRecipeForm({ closeModal }) {
     console.log(newRecipe)
     return (
         <div className="recipe-form">
-            <div className="recipe-header">
+            <header className="recipe-header">
                 <h1>ADD RECIPE</h1>
-                <div className="fav-trash">
+                <nav className="fav-trash">
                     ❤️ 🗑️
-                </div>
-            </div>
+                </nav>
+            </header>
             <form onSubmit={handleSubmit}>
                 <input
                     required
@@ -48,14 +52,34 @@ export default function NewRecipeForm({ closeModal }) {
                     onChange={handleChange}
                     type="text"
                     placeholder="Image URL" />
-                <input required type="number" placeholder="Servings" />
-                <select required name="Meal" id="meal">
+                <input
+                    required
+                    name="servings"
+                    value={newRecipe.serving}
+                    onChange={handleChange}
+                    type="number"
+                    placeholder="Servings" />
+                <select
+                    required
+                    name="Meal"
+                    // value={newRecipe.meal}
+                    onChange={handleChange}
+                    defaultValue={''}
+                    id="meal">
                     <option disabled value="">Select Meal</option>
                     <option value="Breakfast">Breakfast</option>
                     <option value="Lunch">Lunch</option>
                     <option value="Dinner">Dinner</option>
                 </select>
-                <textarea required name="directions" id="directions" cols="30" rows="10"></textarea>
+                <textarea
+                    required
+                    onChange={handleChange}
+                    value={newRecipe.description}
+                    name="directions"
+                    id="directions"
+                    cols="30"
+                    rows="10"
+                />
                 <button type="reset" onClick={recipeCancel}>Cancel</button>
                 <button type="submit" >Complete</button>
             </form>

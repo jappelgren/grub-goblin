995c78942743a43efae6c06c78ff357e07c872dd
Justin Appelgren <justinappelgren@gmail.com>
2021-02-19 22:38:50 -0600

    Styled recipe import form

--- src/components/RecipeImportForm/RecipeImportForm.js
+++ src/components/RecipeImportForm/RecipeImportForm.js
@@ -12,11 +12,18 @@ export default function RecipeImportForm() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
 
-            <input value={importUrl} onChange={(event) => setImportUrl(event.target.value)} type="url" placeholder="Enter Recipe URL" />
-            <button className="goblin-button" type="submit">Submit</button>
-            <button className="goblin-button" onClick={() => dispatch({ type: 'CLOSE_RECIPE_IMPORT' })} type="button">Cancel</button>
-        </form>
+        <div className="import-form">
+            <form onSubmit={handleSubmit}>
+                <div className="import-input-container edit-main">
+                    <input className="" value={importUrl} onChange={(event) => setImportUrl(event.target.value)} type="url" placeholder="Enter Recipe URL" />
+                </div>
+                <div className="modal-button-container edit-buttons">
+                    <button className="goblin-button" type="submit">Submit</button>
+                    <button className="goblin-button" onClick={() => dispatch({ type: 'CLOSE_RECIPE_IMPORT' })} type="button">Cancel</button>
+                </div>
+            </form >
+        </div>
+
     );
 }

702a377ecb86a88b244e29996a8a6dd668883f22
Justin Appelgren <justinappelgren@gmail.com>
2021-02-19 20:55:23 -0600

    Styled edit recipe

--- src/components/EditRecipe/EditRecipe.js
+++ src/components/EditRecipe/EditRecipe.js
@@ -53,25 +53,31 @@ export default function EditRecipe({ setEditMode, editMode, handleFav, handleDel
                 <main className="recipe-modal-main edit-main modal-fade">
                     <div className="ingr-pic-meal-tag-container">
                         <div className="recipe-modal-ingredients">
-                            <input
-                                required
-                                name="recipe_name"
-                                type="text"
-                                onChange={handleChange}
-                                value={newRecipe.recipe_name}
-                                placeholder="Recipe Name" />
+                            <div className="input-label-container">
+                                <label className="label" htmlFor="recipe_name">Recipe Name</label>
+                                <input
+                                    required
+                                    name="recipe_name"
+                                    type="text"
+                                    onChange={handleChange}
+                                    value={newRecipe.recipe_name}
+                                    placeholder="Recipe Name" />
+                            </div>
                             {newRecipe.ingredient.map((ingr, i) => {
                                 if (!ingr.delete)
                                     return (
                                         <div className="ingredient-minus-container" key={i}>
-                                            <input
-                                                required
-                                                type="text"
-                                                name="ingredients"
-                                                value={ingr.ingredient}
-                                                onChange={handleIngredient(i)}
-                                                placeholder="Ingredient"
-                                            />
+                                            <div className="input-label-container">
+                                                <label className="label" htmlFor="recipe_name">Ingredient</label>
+                                                <input
+                                                    required
+                                                    type="text"
+                                                    name="ingredients"
+                                                    value={ingr.ingredient}
+                                                    onChange={handleIngredient(i)}
+                                                    placeholder="Ingredient"
+                                                />
+                                            </div>
                                             <img
                                                 name="remove"
                                                 onClick={handleIngredient(i)}
@@ -87,43 +93,55 @@ export default function EditRecipe({ setEditMode, editMode, handleFav, handleDel
                             </div>
                         </div>
                         <div className="recipe-modal-photo-nutrition">
-                            <input
-                                name="photo"
-                                value={newRecipe.photo}
-                                onChange={handleChange}
-                                type="text"
-                                placeholder="Image URL" />
-                            <input
-                                required
-                                name="servings"
-                                value={newRecipe.servings}
-                                onChange={handleChange}
-                                type="number"
-                                placeholder="Servings" />
-                            <select
-                                required
-                                name="meal"
-                                // value={newRecipe.meal}
-                                onChange={handleChange}
-                                defaultValue={newRecipe.meal}
-                                id="meal">
-                                <option disabled value="">Select Meal</option>
-                                <option value="Breakfast">Breakfast</option>
-                                <option value="Lunch">Lunch</option>
-                                <option value="Dinner">Dinner</option>
-                            </select>
+                            <div className="input-label-container">
+                                <label className="label" htmlFor="recipe_name">Directions</label>
+                                <input
+                                    name="photo"
+                                    value={newRecipe.photo}
+                                    onChange={handleChange}
+                                    type="text"
+                                    placeholder="Image URL" />
+                            </div>
+                            <div className="input-label-container">
+                                <label className="label" htmlFor="recipe_name">Servings</label>
+                                <input
+                                    required
+                                    name="servings"
+                                    value={newRecipe.servings}
+                                    onChange={handleChange}
+                                    type="number"
+                                    placeholder="Servings" />
+                            </div>
+                            <div className="input-label-container">
+                                <label className="label" htmlFor="recipe_name">Meal</label>
+                                <select
+                                    required
+                                    name="meal"
+                                    onChange={handleChange}
+                                    defaultValue={newRecipe.meal}
+                                    className="meal-select"
+                                    id="meal">
+                                    <option disabled value="">Select Meal</option>
+                                    <option value="Breakfast">Breakfast</option>
+                                    <option value="Lunch">Lunch</option>
+                                    <option value="Dinner">Dinner</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                     <div className="text-area-directions">
-                        <textarea
-                            required
-                            onChange={handleChange}
-                            value={newRecipe.directions}
-                            name="directions"
-                            id="directions"
-                        />
+                        <div className="input-label-container">
+                            <label className="label" htmlFor="recipe_name">Directions</label>
+                            <textarea
+                                required
+                                onChange={handleChange}
+                                value={newRecipe.directions}
+                                name="directions"
+                                id="directions"
+                            />
+                        </div>
                     </div>
-                    <div className="modal-button-container">
+                    <div className="modal-button-container edit-buttons">
                         <button className="goblin-button" type="button" onClick={() => setEditMode(!editMode)}>CANCEL</button>
                         <button className="goblin-button" type="submit" >COMPLETE</button>
                     </div>
